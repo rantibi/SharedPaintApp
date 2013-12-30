c6fc20b78f7f1e8f1210e7d2cc77285d1dd15c76
ran <runtibi@gmail.com>
2013-12-30 19:45:17 +0200

    ed

--- gen/com/example/myfirstapp/R.java
+++ gen/com/example/myfirstapp/R.java
@@ -13,9 +13,15 @@ public final class R {
     public static final class color {
         public static final int actionbar_text=0x7f040001;
         public static final int pressed_example=0x7f040000;
+        public static final int tab1_bg=0x7f040002;
+        public static final int tab2_bg=0x7f040003;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
+
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
          */
         public static final int activity_horizontal_margin=0x7f050000;
         public static final int activity_vertical_margin=0x7f050001;
@@ -44,65 +50,80 @@ public final class R {
         public static final int progress_primary_example=0x7f020014;
         public static final int progress_secondary_example=0x7f020015;
         public static final int selectable_background_example=0x7f020016;
-        public static final int spinner_ab_default_example=0x7f020017;
-        public static final int spinner_ab_disabled_example=0x7f020018;
-        public static final int spinner_ab_focused_example=0x7f020019;
-        public static final int spinner_ab_pressed_example=0x7f02001a;
-        public static final int spinner_background_ab_example=0x7f02001b;
-        public static final int tab_indicator_ab_example=0x7f02001c;
-        public static final int tab_selected_example=0x7f02001d;
-        public static final int tab_selected_focused_example=0x7f02001e;
-        public static final int tab_selected_pressed_example=0x7f02001f;
-        public static final int tab_unselected_example=0x7f020020;
-        public static final int tab_unselected_focused_example=0x7f020021;
-        public static final int tab_unselected_pressed_example=0x7f020022;
+        public static final int selected_color_button=0x7f020017;
+        public static final int spinner_ab_default_example=0x7f020018;
+        public static final int spinner_ab_disabled_example=0x7f020019;
+        public static final int spinner_ab_focused_example=0x7f02001a;
+        public static final int spinner_ab_pressed_example=0x7f02001b;
+        public static final int spinner_background_ab_example=0x7f02001c;
+        public static final int tab_indicator_ab_example=0x7f02001d;
+        public static final int tab_selected_example=0x7f02001e;
+        public static final int tab_selected_focused_example=0x7f02001f;
+        public static final int tab_selected_pressed_example=0x7f020020;
+        public static final int tab_unselected_example=0x7f020021;
+        public static final int tab_unselected_focused_example=0x7f020022;
+        public static final int tab_unselected_pressed_example=0x7f020023;
     }
     public static final class id {
-        public static final int action_search=0x7f090006;
-        public static final int action_settings=0x7f090005;
-        public static final int edit_message=0x7f090001;
-        public static final int edit_message1=0x7f090003;
-        public static final int show_message=0x7f090000;
-        public static final int tv_tab1=0x7f090002;
-        public static final int tv_tab2=0x7f090004;
+        public static final int action_cancel_selected=0x7f090014;
+        public static final int action_settings=0x7f090013;
+        public static final int draw_view=0x7f090000;
+        public static final int paint_color_picker_view=0x7f090006;
+        public static final int paint_menu=0x7f090001;
+        public static final int select_shape_circle=0x7f090010;
+        public static final int select_shape_fill=0x7f09000e;
+        public static final int select_shape_line=0x7f09000d;
+        public static final int select_shape_pencil=0x7f09000c;
+        public static final int select_shape_rectangle=0x7f09000f;
+        public static final int select_shape_text=0x7f090012;
+        public static final int select_shape_triangle=0x7f090011;
+        public static final int select_style_fill=0x7f09000a;
+        public static final int select_style_stroke=0x7f090008;
+        public static final int select_style_stroke_and_fill=0x7f090009;
+        public static final int selected_color_button=0x7f090005;
+        public static final int selected_shape_button=0x7f090002;
+        public static final int selected_style_button=0x7f090004;
+        public static final int shape_scrool_view=0x7f09000b;
+        public static final int stroke_width_seek_bar=0x7f090003;
+        public static final int style_scrool_view=0x7f090007;
     }
     public static final class layout {
-        public static final int activity_display_message=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int tab1=0x7f030002;
-        public static final int tab2=0x7f030003;
+        public static final int draw_paint_activitiy=0x7f030000;
     }
     public static final class menu {
-        public static final int display_message=0x7f080000;
-        public static final int main=0x7f080001;
+        public static final int open_gles20=0x7f080000;
     }
     public static final class string {
-        public static final int action_search=0x7f060007;
-        public static final int action_settings=0x7f060003;
+        public static final int action_settings=0x7f060008;
         public static final int app_name=0x7f060000;
-        public static final int button_send=0x7f060002;
-        public static final int edit_message=0x7f060001;
-        public static final int hello_world=0x7f060006;
-        public static final int tab1=0x7f060008;
-        public static final int tab2=0x7f060009;
-        public static final int title_activity_display_message=0x7f060005;
-        public static final int title_activity_main=0x7f060004;
+        public static final int cancel=0x7f060009;
+        public static final int circle=0x7f060007;
+        public static final int fill=0x7f06000c;
+        public static final int line=0x7f060004;
+        public static final int pencil=0x7f060003;
+        public static final int rect=0x7f060002;
+        public static final int stroke=0x7f06000b;
+        public static final int stroke_and_fill=0x7f06000a;
+        public static final int text=0x7f060006;
+        public static final int title_draw_board=0x7f060001;
+        public static final int triangle=0x7f060005;
     }
     public static final class style {
-        public static final int ActionBar_Solid_Example=0x7f070001;
-        public static final int ActionBar_Transparent_Example=0x7f070003;
-        public static final int ActionBarTabStyle_Example=0x7f070006;
+        public static final int ActionBar_Solid_Example=0x7f070002;
+        public static final int ActionBar_Transparent_Example=0x7f070004;
+        public static final int ActionBarTabStyle_Example=0x7f070007;
         /**  ActionBar title text 
          */
-        public static final int ActionBarTitleText=0x7f070002;
-        public static final int ActionButton_CloseMode_Example=0x7f070009;
-        public static final int DropDownListView_Example=0x7f070005;
-        public static final int DropDownNav_Example=0x7f070007;
-        public static final int PopupMenu_Example=0x7f070004;
-        public static final int ProgressBar_Example=0x7f070008;
-        public static final int Theme_Example=0x7f070000;
+        public static final int ActionBarTitleText=0x7f070003;
+        public static final int ActionButton_CloseMode_Example=0x7f07000a;
+        public static final int DropDownListView_Example=0x7f070006;
+        public static final int DropDownNav_Example=0x7f070008;
+        public static final int PopupMenu_Example=0x7f070005;
+        public static final int ProgressBar_Example=0x7f070009;
+        public static final int Theme_Example=0x7f070001;
         /**  this style is only referenced in a Light.DarkActionBar based theme 
          */
-        public static final int Theme_Example_Widget=0x7f07000a;
+        public static final int Theme_Example_Widget=0x7f07000b;
+        public static final int selected_color_button=0x7f070000;
     }
 }
